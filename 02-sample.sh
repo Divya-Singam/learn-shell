f1584d52c748d0b19c47213c2fccc0c2df1b7b4d
Divya <sreedivya1206@gmail.com>
2023-06-15 22:13:13 +1000

    Second shell script

--- 02-sample.sh
+++ 02-sample.sh
@@ -3,3 +3,9 @@ pwd
 yum list installed|grep vim
 # comment
 echo Hello Divya
+echo -e "\e[31mHello Divya in Red Color\e[0m"
+echo -e "\e[32mHello Divya in Green Color\e[0m"
+echo -e "\e[33mHello Divya in Yellow Color\e[0m"
+echo -e "\e[34mHello Divya in Blue Color\e[0m"
+echo -e "\e[35mHello Divya in Magenta Color\e[0m"
+echo -e "\e[36mHello Divya in Cyan Color\e[0m"
